8c76ccdef05eedf109cc0b90ce0be12faa810a12
JAYADI <hostinggogo5@gmail.com>
2026-06-08 15:05:40 +0800

    Update gasUrl in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.GITHUB_CONFIG = {
   repo:   "ortu",
   branch: "main",
   token:  "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",       
-  gasUrl: "https://script.google.com/macros/s/AKfycby3_7Wg5LJGAbZLizbdSFe4oMKspA0U954WR9ue3eTXsBtOYuLwun_S2W7Y_8XdAjKinA/exec",   //pass    
+  gasUrl: "https://script.google.com/macros/s/AKfycbwVv_BtiKhpO48f2jQ8_WYKBRa0PT8Lf5dHFvt7oi8qnkX5HKZd5vDGBwO4bhoSC9q5/exec",   //pass    
 };
