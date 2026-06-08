6afc6652ae006a3c9f68317f381acde527949da2
JAYADI <hostinggogo5@gmail.com>
2026-06-08 15:08:13 +0800

    Remove sensitive data from GITHUB_CONFIG
    Removed sensitive GitHub configuration details.

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 window.GITHUB_CONFIG = {
-  owner:  "hostinger05",
-  repo:   "ortu",
-  branch: "main",
-  token:  "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",       
+  
   gasUrl: "https://script.google.com/macros/s/AKfycbwVv_BtiKhpO48f2jQ8_WYKBRa0PT8Lf5dHFvt7oi8qnkX5HKZd5vDGBwO4bhoSC9q5/exec",   //pass    
 };
