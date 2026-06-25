1733fa650c2906ac1f0b6ce5242c150474588e57
JAYADI <hostinggogo5@gmail.com>
2026-06-25 12:17:30 +0800

    [portal] Update datasiswa.js via Apps Script

--- datasiswa.js
+++ datasiswa.js
@@ -3,7 +3,7 @@
  * File ini di-generate otomatis oleh upload_datasiswa.gs
  * ⚠️  Jangan edit manual — gunakan script upload untuk memperbarui.
  *
- * Terakhir diperbarui: 2026-06-25 12:16:26
+ * Terakhir diperbarui: 2026-06-25 12:17:10
  */
 
 window.SISWA_DATA = {
