67a36a6cbf924f36abdc1f3fd6ed401735d561d3
JAYADI <hostinggogo5@gmail.com>
2026-06-28 09:40:51 +0800

    [portal] Update datasiswa.js (GANJIL+GENAP) via Apps Script

--- datasiswa.js
+++ datasiswa.js
@@ -2,7 +2,7 @@
  * DATABASE SISWA — Portal Akademik SMAN 5 Pinrang
  * Di-generate otomatis oleh upload_datasiswa.gs
  * ⚠️  Jangan edit manual — gunakan script upload untuk memperbarui.
- * Terakhir diperbarui: 2026-06-28 09:35:28
+ * Terakhir diperbarui: 2026-06-28 09:40:31
  */
 
 window.SISWA_DATA = {
